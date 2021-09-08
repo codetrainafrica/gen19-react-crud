b53cd127595b81402b48b45b7a91a81794fb2b39
Anthony Obaze <toneyobaze@gmail.com>
2021-09-08 09:36:51 +0000

    add form to UserForm

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function UserForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
-    <div>
-      <h>User form</h>
-    </div>
+    <form>
+      <input
+        type="text"
+        name="fullname"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input type="submit" />
+    </form>
   );
 }
 export default UserForm;
