d624779c064bd00395e2ee3570819ffbe1a60b05
Anthony Obaze <toneyobaze@gmail.com>
2021-09-08 10:27:40 +0000

    add users state, function to addUser

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import UserForm from "./components/UserForm";
 import UserList from "./components/UserList";
 
 function App() {
+  //users state array
+  const [users, setUsers] = useState([]);
+
+  //function to add a new user
+  function addUser(user) {
+    //makes a copy of the current users state
+    //and add a new user.
+    setUsers([...users, user]);
+  }
+
   return (
     <div className="App">
-      <UserForm />
-      <UserList />
+      <UserForm addUser={addUser} />
+      <UserList data={users} />
     </div>
   );
 }
