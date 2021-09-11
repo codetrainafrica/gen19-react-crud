19bb197fa70eed97fae23ce192b7e0c64aea7c0d
Anthony Obaze <toneyobaze@gmail.com>
2021-09-11 08:34:13 +0000

    reset app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import UserForm from "./components/UserForm";
-import UserList from "./components/UserList";
 
 function App() {
-  //users state array
-  const [users, setUsers] = useState([]);
-
-  //function to add a new user
-  function addUser(user) {
-    //makes a copy of the current users state
-    //and add a new user.
-    setUsers([...users, user]);
-  }
-
-  return (
-    <div className="App">
-      <UserForm addUser={addUser} />
-      <UserList data={users} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
