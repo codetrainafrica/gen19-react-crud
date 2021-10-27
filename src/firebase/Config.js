5534711cc7681e86af8a655f1f8701803cab151c
nana <comfortappiah.ca@gmail.com>
2021-10-27 09:13:59 +0000

    firebase setup

---
+++ src/firebase/Config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyAflQ9LU0JfJLIhDrBcx-ojmKP2yndNgHw",
+	authDomain: "school-app-3598e.firebaseapp.com",
+	projectId: "school-app-3598e",
+	storageBucket: "school-app-3598e.appspot.com",
+	messagingSenderId: "621633493097",
+	appId: "1:621633493097:web:0fe6cfc387e244969f2af8",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,17 @@
-import {createStore} from "redux";
-import usersReducers from "../reducers/usersReducers"
+import { createStore, applyMiddleware, compose } from "redux";
+import usersReducers from "../reducers/usersReducers";
+import firebase from "../firebase/Config";
+import { getFirebase, reduxReactFirebase } from "react-redux-firebase";
+import { getFirestore, reduxFirestore } from "redux-firestore";
+import thunk from "redux-thunk";
 
-const store = createStore(usersReducers)
-
+const store = createStore(
+	usersReducers,
+	compose(
+		applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+		reduxReactFirebase(firebase),
+		reduxFirestore(firebase)
+	)
+);
 
 export default store;
